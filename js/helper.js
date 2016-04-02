249d04f5dff86748576f9f82052a9bffdf0a959b
Nikhil Narula <nik.coolest@gmail.com>
2016-04-02 15:06:34 -0400

    Adding all the new files

---
+++ js/helper.js
@@ -0,0 +1,217 @@
+var HTMLnavHeader = '<nav class="navbar navbar-default"><div class="container"></div></nav>';
+var HTMLnavbarHeader = '<div class="navbar-header"><button type="button" class="navbar-toggle collapsed" ' +
+          'data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">' +
+          '<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span>' +
+          '<span class="icon-bar"></span></button></div>';
+var HTMLnavbarCollapse = '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' +
+          '<ul class="nav navbar-nav"><li><a id="1" href="#navbar">Bio</a></li>' +
+          '<li><a id="2" href="#workExperience">Work Experience</a></li>' +
+          '<li><a id="3" href="#projects">Projects</a></li>' +
+          '<li><a id="4" href="#education">Education</a></li>' +
+          '<li><a id="5" href="#mapDiv">Map</a></li>' +
+          '<li><a id="6" href="#lets-connect">Contacts</a></li></ul></div>';
+
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<h3 class="role">%data%</h3><hr>';
+
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+//var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+//var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLwelcomeMsg = '<p class="welcome-message">%data%</p>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><div class="skill-container">' +
+          '<div class="skill-list"><ul id="skills" class="flex-box skills"></ul></div><div class="skill-chart"></div></div>';
+var HTMLchart1 = '<div id="html5" class="chart"><strong>75%</strong><span>HTML5</span></div>';
+var HTMLchart2 = '<div id="javascript" class="chart"><strong>70%</strong><span>JavaScript</span></div>';
+var HTMLchart3 = '<div id="css" class="chart"><strong>80%</strong><span>CSS</span></div>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLmore = '<li class="more"><a class="more-less-link" href="">Show More</a></li>';
+var HTMLless = '<li class="less"><a class="more-less-link" href="">Show Less</a></li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<div id="name">%data%';
+var HTMLworkTitle = ' - %data%</div>';
+var HTMLworkDateWrap = '<div class="work-flex"></div>';
+var HTMLworkDates = '<div class="date-text flex-item">%data%</div>';
+var HTMLworkLocation = '<div class="location-text flex-item">%data%</div>';
+var HTMLworkDescription = '<p class="padding-top10">%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<div id="name">%data%</div>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+//var HTMLprojectImage = '<img src="%data%">';
+//var HTMLprojectClient = '<div class="client-text">Client - %data%</div>';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolDegree = '<div id="name">%data%: ';
+var HTMLschoolName = '%data%</div>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%<br><br></em>';
+
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineURL = '<a href="%data%">';
+var HTMLonlineTitle = '%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div><br>';
+
+var internationalizeButton = '<button>Internationalize</button>';
+var googleMap = '<div id="map"></div>';
+
+var HTMLfooterAnchor = '<a href="%data%" ';
+var HTMLfooterTitle = 'title="%data%" target="_blank">';
+var HTMLfooterIcon = '<i class="fa %data% fa-2x social"></i></a>';
+
+var map;
+function initializeMap() {
+
+  var locations;
+
+  var mapOptions = {
+    disableDefaultUI: true
+  };
+
+  /*
+  For the map to be displayed, the googleMap var must be
+  appended to #mapDiv in resumeBuilder.js.
+  */
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+
+  /*
+  locationFinder() returns an array of every location string from the JSONs
+  written for bio, education, and work.
+  */
+  function locationFinder() {
+
+    // initializes an empty array
+    var locations = [];
+
+    // adds the single location property from bio to the locations array
+    locations.push(bio.contacts.location);
+
+    // iterates through school locations and appends each location to
+    // the locations array.
+    education.schools.forEach(function(school){
+      locations.push(school.location);
+    });
+
+    // iterates through work locations and appends each location to
+    // the locations array.
+    work.jobs.forEach(function(job){
+      locations.push(job.location);
+    });
+
+    return locations;
+  }
+
+  /*
+  createMapMarker(placeData) reads Google Places search results to create map pins.
+  placeData is the object returned from search results containing information
+  about a single location.
+  */
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var name = placeData.formatted_address;   // name of the place from the place service
+    var bounds = window.mapBounds;            // current boundaries of the map window
+
+    // marker is an object with additional data about the pin for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name,
+    });
+
+    // infoWindows are the little helper windows that open when you click
+    // or hover over a pin on a map. They usually contain more information
+    // about a location.
+    var infoWindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      // your code goes here!
+      infoWindow.open(map,marker);
+    });
+
+    // this is where the pin actually gets added to the map.
+    // bounds.extend() takes in a map location object
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  }
+
+  /*
+  callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location.
+  */
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0]);
+    }
+  }
+
+  /*
+  pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location
+  */
+  function pinPoster(locations) {
+
+    // creates a Google place search service object. PlacesService does the work of
+    // actually searching for location data.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterates through the array of locations, creates a search object for each location
+      locations.forEach(function(place){
+      // the search request object
+      var request = {
+        query: place
+      };
+
+      // Actually searches the Google Maps API for location data and runs the callback
+      // function with the search results after each search.
+      service.textSearch(request, callback);
+    });
+  }
+
+  // Sets the boundaries of the map based on pin locations
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  // locations is an array of location strings returned from locationFinder()
+  locations = locationFinder();
+
+  // pinPoster(locations) creates pins on the map for each location in
+  // the locations array
+  pinPoster(locations);
+
+}
+
+window.addEventListener('load', initializeMap);
+
+//function to smooth scrolling from navbar
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
